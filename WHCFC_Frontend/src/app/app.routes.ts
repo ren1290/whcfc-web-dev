3b1d3ee7fbf523a13306904563dd97fa61219023
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-14 13:08:03 -0400

    Created starting volunteer page and linked it to the router

--- WHCFC_Frontend/src/app/app.routes.ts
+++ WHCFC_Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { VolunteerComponent } from './pages/volunteer/volunteer.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'volunteer', component: VolunteerComponent}
 ];
