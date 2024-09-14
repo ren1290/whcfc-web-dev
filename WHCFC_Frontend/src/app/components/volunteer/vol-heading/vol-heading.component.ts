cc00906238f66d3ce4cc242c604f8744abe053cb
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-14 14:30:18 -0400

    Implemented Volunteer page header content only

---
+++ WHCFC_Frontend/src/app/components/volunteer/vol-heading/vol-heading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vol-heading',
+  standalone: true,
+  imports: [],
+  templateUrl: './vol-heading.component.html',
+  styleUrl: './vol-heading.component.css'
+})
+export class VolHeadingComponent {
+
+}
