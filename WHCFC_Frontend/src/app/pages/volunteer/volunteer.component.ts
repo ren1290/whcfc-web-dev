75fcd93a87699ff2fd6c2ee926915f7aa698c384
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-14 21:02:04 -0400

    Importing style sheets for the best UI designs and properly using Angular to link components

--- WHCFC_Frontend/src/app/pages/volunteer/volunteer.component.ts
+++ WHCFC_Frontend/src/app/pages/volunteer/volunteer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { VolHeadingComponent } from '../../components/volunteer/vol-heading/vol-heading.component';
+import { VolOpportunitiesComponent } from '../../components/volunteer/vol-opportunities/vol-opportunities.component';
 
 @Component({
   selector: 'app-volunteer',
   standalone: true,
-  imports: [VolHeadingComponent],
+  imports: [VolHeadingComponent, VolOpportunitiesComponent],
   templateUrl: './volunteer.component.html',
   styleUrl: './volunteer.component.css'
 })
