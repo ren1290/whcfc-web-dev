bdc594e1e9a18450ff30e58625ff01f75832e78e
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-14 20:31:01 -0400

    Created component for Volunteer Opportunities section

---
+++ WHCFC_Frontend/src/app/components/volunteer/vol-opportunities/vol-opportunities.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vol-opportunities',
+  standalone: true,
+  imports: [],
+  templateUrl: './vol-opportunities.component.html',
+  styleUrl: './vol-opportunities.component.css'
+})
+export class VolOpportunitiesComponent {
+
+}
