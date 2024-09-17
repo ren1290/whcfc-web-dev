bcb6604c35552df9e88ee8825b8e506b0b032fdc
Tony Klinpon <thitiklinpon@gmail.com>
2024-09-17 11:08:58 -0400

    Add contact page content and integrate TailwindCSS

--- WHCFC_Frontend/src/app/app.component.ts
+++ WHCFC_Frontend/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
