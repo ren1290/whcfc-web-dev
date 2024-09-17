49eb7903669cb21cae295483f252e110311e0503
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-17 19:55:35 -0400

    Fixed volunteer button problem - works correctly now

--- WHCFC_Frontend/src/app/app.routes.ts
+++ WHCFC_Frontend/src/app/app.routes.ts
@@ -1,8 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { VolunteerComponent } from './pages/volunteer/volunteer.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'volunteer', component: VolunteerComponent}
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+  export class AppRoutingModule { }
