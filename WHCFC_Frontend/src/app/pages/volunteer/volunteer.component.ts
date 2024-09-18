ccc0ca17afdf612beacc067f80ef3611dfe60e9c
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-18 13:41:06 -0400

    Linked component with rest of volunteer page

--- WHCFC_Frontend/src/app/pages/volunteer/volunteer.component.ts
+++ WHCFC_Frontend/src/app/pages/volunteer/volunteer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { VolHeadingComponent } from '../../components/volunteer/vol-heading/vol-heading.component';
 import { VolOpportunitiesComponent } from '../../components/volunteer/vol-opportunities/vol-opportunities.component';
 import { VolTestimonialsComponent } from '../../components/volunteer/vol-testimonials/vol-testimonials.component';
+import { VolGetInvolvedComponent } from '../../components/volunteer/vol-get-involved/vol-get-involved.component';
 
 @Component({
   selector: 'app-volunteer',
   standalone: true,
-  imports: [VolHeadingComponent, VolOpportunitiesComponent, VolTestimonialsComponent],
+  imports: [VolHeadingComponent, VolOpportunitiesComponent, VolTestimonialsComponent, VolGetInvolvedComponent],
   templateUrl: './volunteer.component.html',
   styleUrl: './volunteer.component.css'
 })
