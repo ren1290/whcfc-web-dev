8611295c3b641d75f58b1c4aba21f10028817dae
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-18 14:59:06 -0400

    Implemented apply now section

---
+++ WHCFC_Frontend/src/app/components/volunteer/vol-apply/vol-apply.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vol-apply',
+  standalone: true,
+  imports: [],
+  templateUrl: './vol-apply.component.html',
+  styleUrl: './vol-apply.component.css'
+})
+export class VolApplyComponent {
+
+}
