0c761221dc6404ebc5f40f520bc5b871a8e8f8bd
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-19 17:26:11 -0400

    Adding form to allow users to enter unanswered questions

--- WHCFC_Frontend/src/app/pages/faq/faq.component.ts
+++ WHCFC_Frontend/src/app/pages/faq/faq.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FaqHeadingComponent } from '../../components/faq/faq-heading/faq-heading.component';
 import { FaqCoreComponent } from '../../components/faq/faq-core/faq-core.component';
+import { FaqMissComponent } from '../../components/faq/faq-miss/faq-miss.component';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [FaqHeadingComponent, FaqCoreComponent],
+  imports: [FaqHeadingComponent, FaqCoreComponent, FaqMissComponent],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
