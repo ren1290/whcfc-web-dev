258f497027c49199618692e560f308894fa6892a
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-19 16:45:01 -0400

    Started to create core for FAQs drop down

---
+++ WHCFC_Frontend/src/app/components/faq/faq-core/faq-core.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-core',
+  standalone: true,
+  imports: [],
+  templateUrl: './faq-core.component.html',
+  styleUrl: './faq-core.component.css'
+})
+export class FaqCoreComponent {
+
+}
--- WHCFC_Frontend/src/app/pages/faq/faq.component.ts
+++ WHCFC_Frontend/src/app/pages/faq/faq.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FaqHeadingComponent } from '../../components/faq/faq-heading/faq-heading.component';
+import { FaqCoreComponent } from '../../components/faq/faq-core/faq-core.component';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [FaqHeadingComponent],
+  imports: [FaqHeadingComponent, FaqCoreComponent],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
