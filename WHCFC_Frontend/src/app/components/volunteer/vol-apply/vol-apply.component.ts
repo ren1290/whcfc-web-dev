e5828b690a0eb60663462660365b4effd57296d4
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-21 11:58:02 -0400

    Setting up front end Angular form to handle form submissions

--- WHCFC_Frontend/src/app/components/volunteer/vol-apply/vol-apply.component.ts
+++ WHCFC_Frontend/src/app/components/volunteer/vol-apply/vol-apply.component.ts
@@ -1,12 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { VolApplyInfoService } from '../../../vol-apply-info.service';
 
 @Component({
   selector: 'app-vol-apply',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './vol-apply.component.html',
   styleUrl: './vol-apply.component.css'
 })
 export class VolApplyComponent {
+  volApplyInfo = {
+    firstName: '',
+    lastName: '',
+    emailAddress: '',
+    phoneNumber: '',
+    category: '',
+    message: ''
+  };
 
+  constructor(private volService: VolApplyInfoService) {}
+
+  onSubmit() {
+    this.volService.sendContactForm(this.volService).subscribe(response => {
+      console.log('Form submitted successfully', response);
+    }, error => {
+      console.log('Error sending message.', error);
+    }
+  );
+  }
 }
